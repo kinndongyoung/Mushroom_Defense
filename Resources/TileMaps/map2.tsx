bd36cc78e50fea71f3323c0b6a38fa73d497a559
kinndongyoung <kicl6657@naver.com>
2019-05-30 20:59:26 +0900

    Create map2.tsx

---
+++ Resources/TileMaps/map2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="map2" tilewidth="80" tileheight="80" spacing="1" margin="1" tilecount="1" columns="1">
+ <image source="../map2.png" width="81" height="81"/>
+</tileset>
