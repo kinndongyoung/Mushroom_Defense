b83a7061632f7771dbd0396327149f0120320e1e
kinndongyoung <kicl6657@naver.com>
2019-05-30 20:59:25 +0900

    Create map1.tsx

---
+++ Resources/TileMaps/map1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="map1" tilewidth="80" tileheight="80" tilecount="1" columns="1">
+ <image source="../map1.png" width="80" height="80"/>
+</tileset>
