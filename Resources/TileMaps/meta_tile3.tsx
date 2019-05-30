d9809095a87c17c329adf07cff167ab2d20d536d
kinndongyoung <kicl6657@naver.com>
2019-05-30 20:59:06 +0900

    Create meta_tile3.tsx

---
+++ Resources/TileMaps/meta_tile3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="meta_tile2" tilewidth="64" tileheight="64" spacing="1" margin="2" tilecount="2" columns="2">
+ <image source="meta_tile2.png" width="134" height="68"/>
+</tileset>
