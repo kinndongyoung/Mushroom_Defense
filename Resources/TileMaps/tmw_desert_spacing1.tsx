9ef6b2944e8d133a269d60078c703b8203c6b90b
kinndongyoung <kicl6657@naver.com>
2019-05-30 20:58:54 +0900

    Create tmw_desert_spacing1.tsx

---
+++ Resources/TileMaps/tmw_desert_spacing1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="tmw_desert_spacing1" tilewidth="64" tileheight="64" spacing="2" margin="2" tilecount="48" columns="8">
+ <image source="tmw_desert_spacing1.png" width="530" height="398"/>
+</tileset>
