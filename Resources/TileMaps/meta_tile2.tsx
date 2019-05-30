04539d8e74f474c54ae6dfac3a887ce29efdbef1
kinndongyoung <kicl6657@naver.com>
2019-05-30 20:58:58 +0900

    Create meta_tile2.tsx

---
+++ Resources/TileMaps/meta_tile2.tsx
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="meta_tile" tilewidth="32" tileheight="32" spacing="1" margin="1" tilecount="2" columns="2">
+ <image source="meta_tile.png/meta_tile.png" width="67" height="34"/>
+ <tile id="0" type="strring">
+  <properties>
+   <property name="값" value="obstacle"/>
+   <property name="속성이름" value="collisionType"/>
+   <property name="타입" value="string"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="값" value="item"/>
+   <property name="속성이름" value="collisionType"/>
+   <property name="타입" value="string"/>
+  </properties>
+ </tile>
+</tileset>
